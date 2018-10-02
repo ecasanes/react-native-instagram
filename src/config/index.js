b84534dd839ca292253ba750ece4d64fbcdf4bd8
Ernest Oliver Casanes <ecasanes@gmail.com>
2018-10-03 00:42:40 +0800

    additional assets

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+export default {
+  images: {
+    heartIcon: require('../../assets/heart-icon.png'),
+    commentIcon: require('../../assets/comment-icon.png'),
+    shareIcon: require('../../assets/share-icon.png'),
+    instagramTextLogo: require('../../assets/instagram-text-logo.png')
+  },
+  styleConstants: {
+    rowHeight: 50
+  }
+}
